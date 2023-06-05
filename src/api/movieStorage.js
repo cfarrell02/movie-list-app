4373af945718dc6ae3dcf0d1dfda2001f338b9c3
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2023-06-06 00:27:17 +0100

    Added caching

--- src/api/movieStorage.js
+++ src/api/movieStorage.js
@@ -1,6 +1,9 @@
 import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase-config';
 
+// Local cache to store movies data
+let moviesCache = [];
+
 // Function to add a movie
 export const addMovie = async (movie) => {
   try {
@@ -14,11 +17,20 @@ export const addMovie = async (movie) => {
 // Function to get all movies
 export const getAllMovies = async () => {
   try {
+    // Check if movies data exists in the cache
+    if (moviesCache.length > 0) {
+      return moviesCache;
+    }
+
     const querySnapshot = await getDocs(collection(db, 'movies'));
     const movies = [];
     querySnapshot.forEach((doc) => {
       movies.push({ id: doc.id, ...doc.data() });
     });
+
+    // Update the cache with the new movies data
+    moviesCache = movies;
+
     return movies;
   } catch (error) {
     throw new Error('Error getting movies from Firestore.');
@@ -30,6 +42,14 @@ export const updateMovie = async (id, updatedMovie) => {
   try {
     const movieRef = doc(db, 'movies', id);
     await updateDoc(movieRef, updatedMovie);
+
+    // Update the movie in the cache
+    moviesCache = moviesCache.map((movie) => {
+      if (movie.id === id) {
+        return { id, ...updatedMovie };
+      }
+      return movie;
+    });
   } catch (error) {
     throw new Error('Error updating movie in Firestore.');
   }
@@ -40,6 +60,9 @@ export const deleteMovie = async (id) => {
   try {
     const movieRef = doc(db, 'movies', id);
     await deleteDoc(movieRef);
+
+    // Remove the movie from the cache
+    moviesCache = moviesCache.filter((movie) => movie.id !== id);
   } catch (error) {
     throw new Error('Error deleting movie from Firestore.');
   }
